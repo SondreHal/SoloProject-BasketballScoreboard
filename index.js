1c68040983321bf9199ad5c8ac53c3a1e2d9c8d1
Sondre <sondre-h@hotmail.com>
2023-01-19 15:39:59 +0100

    V3

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 let homeCount = 0;
+let homeScoreContainer = document.querySelector('#home-score-container');
 let homeScore = document.querySelector('#home-score');
+let homeNumBehind = document.querySelector('#home-num-behind');
 
 let guestCount = 0;
+let guestScoreContainer = document.querySelector('#guest-score-container');
 let guestScore = document.querySelector('#guest-score');
+let guestNumBehind = document.querySelector('#guest-num-behind');
 
 let leadingTeamText = document.querySelector('#leading-team-text');
 
@@ -15,28 +19,60 @@ let minutes = 0;
 // HOME SCORE
 function home(num) {
 	homeCount += num;
-	homeScore.textContent = homeCount;
-	checkLead();
+	// CAPS HOME SCORE AT 99
+	if (homeCount > 99) {
+		homeCount = 99;
+		homeScore.textContent = homeCount;
+		checkLead();
+	} else {
+		// DECIDES IF TO ADD 0 BEFORE HOME SCORE OR NOT
+		if (homeCount < 10) {
+			homeScore.textContent = '0' + homeCount;
+			checkLead();
+		} else {
+			homeScore.textContent = homeCount;
+			checkLead();
+		}
+	}
 }
 
 // GUEST SCORE
 function guests(num) {
 	guestCount += num;
-	guestScore.textContent = guestCount;
-	checkLead();
+	// CAPS GUEST SCORE AT 99
+	if (guestCount > 99) {
+		guestCount = 99;
+		guestScore.textContent = guestCount;
+		checkLead();
+	} else {
+		// DECIDES IF TO ADD 0 BEFORE GUEST SCORE OR NOT
+		if (guestCount < 10) {
+			guestScore.textContent = '0' + guestCount;
+			checkLead();
+		} else {
+			guestScore.textContent = guestCount;
+			checkLead();
+		}
+	}
 }
 
 // RESET GAME
 function newGame() {
+	// HOME SCORE
 	homeCount = 0;
-	homeScore.textContent = 0;
+	homeScore.textContent = '00';
+	homeNumBehind.textContent = '00';
 
+	// GUEST SCORE
 	guestCount = 0;
-	guestScore.textContent = 0;
+	guestScore.textContent = '00';
+	guestNumBehind.textContent = '00';
 
+	// SECONDS
 	seconds = 0;
 	timerSeconds.textContent = '0' + seconds;
 
+	// MINUTES
 	minutes = 0;
 	timerMinutes.textContent = '0' + seconds;
 
@@ -46,23 +82,23 @@ function newGame() {
 function checkLead() {
 	if (homeCount > guestCount) {
 		// HOME LEADS
-		homeScore.style.boxShadow = '0px 0px 10px 5px #ff1500';
-		guestScore.style.boxShadow = '0px 0px';
+		homeScoreContainer.style.boxShadow = '0px 0px 10px 5px #ff1500';
+		guestScoreContainer.style.boxShadow = '';
 		leadingTeamText.textContent = 'HOME TEAM IS IN THE LEAD!';
 	} else if (homeCount < guestCount) {
 		// GUEST LEADS
-		homeScore.style.boxShadow = '0px 0px';
-		guestScore.style.boxShadow = '0px 0px 10px 5px #ff1500';
+		homeScoreContainer.style.boxShadow = '';
+		guestScoreContainer.style.boxShadow = '0px 0px 10px 5px #ff1500';
 		leadingTeamText.textContent = 'GUEST TEAM IS IN THE LEAD!';
 	} else if (homeCount === 0 && guestCount === 0) {
 		// NO SCORE
-		homeScore.style.boxShadow = '';
-		guestScore.style.boxShadow = '';
+		homeScoreContainer.style.boxShadow = '';
+		guestScoreContainer.style.boxShadow = '';
 		leadingTeamText.textContent = 'NO TEAM HAS SCORED!';
 	} else {
 		// TIED
-		homeScore.style.boxShadow = '';
-		guestScore.style.boxShadow = '';
+		homeScoreContainer.style.boxShadow = '';
+		guestScoreContainer.style.boxShadow = '';
 		leadingTeamText.textContent = 'BOTH TEAMS ARE TIED!';
 	}
 }
