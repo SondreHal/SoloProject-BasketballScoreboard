8de380e1af019d0fc796aad37b0a0e68736e100f
Sondre <sondre-h@hotmail.com>
2023-01-19 13:20:47 +0100

    v2

--- index.js
+++ index.js
@@ -1,104 +1,96 @@
-let home = document.querySelector("#home");
-let guest = document.querySelector("#guest");
-let homeBg = document.querySelector("#home-bg");
-let guestBg = document.querySelector("#guest-bg");
-let leadingTeam = document.querySelector("#leading-team");
-let timerS = document.querySelector("#timerS");
-let timerM = document.querySelector("#timerM");
-let seconds = 0;
-let minutes = 0;
 let homeCount = 0;
+let homeScore = document.querySelector('#home-score');
+
 let guestCount = 0;
+let guestScore = document.querySelector('#guest-score');
 
-function home1() {
-   homeCount += 1;
-   home.textContent = homeCount;
-   lead();
+let leadingTeamText = document.querySelector('#leading-team-text');
+
+let timerSeconds = document.querySelector('#timerSeconds');
+let seconds = 0;
+
+let timerMinutes = document.querySelector('#timerMinutes');
+let minutes = 0;
+
+// HOME SCORE
+function home(num) {
+	homeCount += num;
+	homeScore.textContent = homeCount;
+	checkLead();
 }
 
-function home2() {
-   homeCount += 2;
-   home.textContent = homeCount;
-   lead();
+// GUEST SCORE
+function guests(num) {
+	guestCount += num;
+	guestScore.textContent = guestCount;
+	checkLead();
 }
 
-function home3() {
-   homeCount += 3;
-   home.textContent = homeCount;
-   lead();
-}
-
-function guests1() {
-   guestCount += 1;
-   guest.textContent = guestCount;
-   lead();
-}
-
-function guests2() {
-   guestCount += 2;
-   guest.textContent = guestCount;
-   lead();
-}
-
-function guests3() {
-   guestCount += 3;
-   guest.textContent = guestCount;
-   lead();
-}
-
+// RESET GAME
 function newGame() {
-   homeCount = 0;
-   guestCount = 0;
-   home.textContent = 0;
-   guest.textContent = 0;
-   lead();
+	homeCount = 0;
+	homeScore.textContent = 0;
+
+	guestCount = 0;
+	guestScore.textContent = 0;
+
+	seconds = 0;
+	timerSeconds.textContent = '0' + seconds;
+
+	minutes = 0;
+	timerMinutes.textContent = '0' + seconds;
+
+	checkLead();
 }
 
-function lead() {
-   if (homeCount > guestCount) {
-      homeBg.style.boxShadow = "0px 0px 20px 5px #FFFF00";
-      guestBg.style.boxShadow = "0px 0px";
-      leadingTeam.textContent = "HOME TEAM IS IN THE LEAD!";
-   } else if (homeCount < guestCount) {
-      homeBg.style.boxShadow = "0px 0px";
-      guestBg.style.boxShadow = "0px 0px 20px 5px #FFFF00";
-      leadingTeam.textContent = "GUEST TEAM IS IN THE LEAD!";
-   } else if (homeCount === 0 && guestCount === 0) {
-      homeBg.style.boxShadow = "0px 0px 0px #FFFF00";
-      guestBg.style.boxShadow = "0px 0px 0px #FFFF00";
-      leadingTeam.textContent = "NO TEAM HAS SCORED!";
-   } else {
-      homeBg.style.boxShadow = "0px 0px 0px #FFFF00";
-      guestBg.style.boxShadow = "0px 0px 0px #FFFF00";
-      leadingTeam.textContent = "BOTH TEAMS ARE TIED!";
-   }
+function checkLead() {
+	if (homeCount > guestCount) {
+		// HOME LEADS
+		homeScore.style.boxShadow = '0px 0px 10px 5px #ff1500';
+		guestScore.style.boxShadow = '0px 0px';
+		leadingTeamText.textContent = 'HOME TEAM IS IN THE LEAD!';
+	} else if (homeCount < guestCount) {
+		// GUEST LEADS
+		homeScore.style.boxShadow = '0px 0px';
+		guestScore.style.boxShadow = '0px 0px 10px 5px #ff1500';
+		leadingTeamText.textContent = 'GUEST TEAM IS IN THE LEAD!';
+	} else if (homeCount === 0 && guestCount === 0) {
+		// NO SCORE
+		homeScore.style.boxShadow = '';
+		guestScore.style.boxShadow = '';
+		leadingTeamText.textContent = 'NO TEAM HAS SCORED!';
+	} else {
+		// TIED
+		homeScore.style.boxShadow = '';
+		guestScore.style.boxShadow = '';
+		leadingTeamText.textContent = 'BOTH TEAMS ARE TIED!';
+	}
 }
-
-lead();
+checkLead();
 
 function incrementSeconds() {
-   seconds += 1;
-   if (seconds < 10) {
-      timerS.innerText = "0" + seconds;
-   } else if (seconds === 60) {
-      seconds = 0;
-      timerS.innerText = "0" + seconds;
-      incrementMinutes();
-   } else {
-      timerS.innerText = seconds;
-   }
+	seconds += 1;
+	if (seconds < 10) {
+		timerSeconds.textContent = '0' + seconds;
+	} else if (seconds === 60) {
+		seconds = 0;
+		timerSeconds.textContent = '0' + seconds;
+		incrementMinutes();
+	} else {
+		timerSeconds.textContent = seconds;
+	}
 }
+setInterval(incrementSeconds, 1000);
 
 function incrementMinutes() {
-   minutes += 1;
-   if (minutes < 10) {
-      timerM.innerText = "0" + minutes;
-   } else if (minutes === 48) {
-      minutes = 0;
-      timerM.innerText = "0" + minutes;
-   } else {
-      timerM.innerText = minutes;
-   }
+	minutes += 1;
+	if (minutes < 10) {
+		timerMinutes.textContent = '0' + minutes;
+	} else if (minutes === 48) {
+		minutes = 0;
+		timerMinutes.textContent = '0' + minutes;
+		newGame();
+	} else {
+		timerMinutes.textContent = minutes;
+	}
 }
-
-let incrementS = setInterval(incrementSeconds, 1000);
